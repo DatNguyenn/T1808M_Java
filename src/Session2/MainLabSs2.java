2c753ae94b06cb045d0cd7c4d2dfb3c57bda5eda
Datnguyenn <datnguyenquang181@gmail.com>
2019-03-11 11:13:41 +0700

    Lab Session2

---
+++ src/Session2/MainLabSs2.java
@@ -0,0 +1,13 @@
+package Session2;
+
+public class MainLabSs2 {
+    public static void main(String[] args)
+    {
+        NhanVien nhanvien1 =new NhanVien();
+        nhanvien1.inputInfo();
+        nhanvien1.printInfo();
+        System.out.println("Tien Thuong Cua Nhan Vien : "+nhanvien1.tinhThuong());
+
+    }
+
+}
